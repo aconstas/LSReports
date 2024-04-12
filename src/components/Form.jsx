d282629ac29d5c5295a9f4c587df62a0ad4b0f98
aconstas <82304192+aconstas@users.noreply.github.com>
2024-04-11 22:19:31 -0700

    add state for checkboxes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,56 @@
-import { FormGroup } from "@mui/material";
-import GrantChecbox from "./GrantCheckbox";
+import { FormGroup, FormControlLabel, Checkbox } from "@mui/material";
 import ExcelInput from "./ExcelInput";
 import CompareButton from "./CompareButton";
+import { useState } from "react";
 
-export default function Form() {
+export default function Form({ setShowResults, setResultsData }) {
+  const [ISF_NAC, setISF_NAC] = useState(false);
+  const [ONECA, setONECA] = useState(false);
+  const [excelText, setExcelText] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("The form was submitted.");
+    const selectTables = [];
+    if (ISF_NAC) {
+      selectTables.push("ISF_NAC Both");
+    }
+    if (ONECA) {
+      selectTables.push("ONECA-C");
+    }
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        {
+          action: "extractData",
+          tables: selectTables,
+          excelText: excelText,
+        },
+        (response) => {
+          setResultsData(response.data);
+          setShowResults(true);
+        }
+      );
+    });
   };
 
   return (
-    <form onSubmit={handleSubmit} sx={{ display: "flex" }}>
+    <form onSubmit={handleSubmit}>
       <FormGroup>
-        <GrantChecbox label={"CDSS"} />
-        <GrantChecbox label={"ONECA-C"} />
+        <FormControlLabel
+          control={<Checkbox />}
+          label={"ISF_NAC Both"}
+          checked={ISF_NAC}
+          onClick={() => setISF_NAC(!ISF_NAC)}
+        />
+        <FormControlLabel
+          control={<Checkbox />}
+          label={"ONECA-C"}
+          checked={ONECA}
+          onClick={() => setONECA(!ONECA)}
+        />
       </FormGroup>
-      <ExcelInput />
+      <ExcelInput excelText={excelText} setExcelText={setExcelText} />
       <CompareButton />
     </form>
   );
