01a2f82367b892230d287521091202977a26b9dc
aconstas <82304192+aconstas@users.noreply.github.com>
2024-04-11 22:18:53 -0700

    render <Results> dynamically based on showResults state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { Typography } from "@mui/material";
 import Form from "./components/Form";
 import Results from "./components/Results";
+import { useState } from "react";
 
 function App() {
+  const [showResults, setShowResults] = useState(false);
+  const [resultsData, setResultsData] = useState({});
   return (
     <>
-      <Typography variant="h4" sx={{ mb: 4, textAlign: "center" }}>
+      <Typography variant="h4" sx={{ mb: 2, textAlign: "center" }}>
         LSReports
       </Typography>
-      <Form />
-      <Results />
+      {!showResults && <Form
+        showResults={showResults}
+        setShowResults={setShowResults}
+        setResultsData={setResultsData}
+      />}
+      {showResults && <Results ExcelAndLSCaseIDs={resultsData} setShowResults={setShowResults}/>}
     </>
   );
 }
