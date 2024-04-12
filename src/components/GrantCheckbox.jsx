42037a75c77540007b55b3784673ac474609c370
aconstas <82304192+aconstas@users.noreply.github.com>
2024-04-11 22:20:59 -0700

    delete unused component

--- src/components/GrantCheckbox.jsx
+++
@@ -1,5 +0,0 @@
-import { Checkbox, FormControlLabel } from "@mui/material";
-
-export default function GrantChecbox({ label }) {
-  return <FormControlLabel control={<Checkbox />} label={label} />;
-}
