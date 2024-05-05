db5c671306d8d5f29cba78655079e5661143506e
aconstas <82304192+aconstas@users.noreply.github.com>
2024-05-04 21:02:50 -0700

    MAJOR UPDATE
    feat: allow users to save grant names to local storage for customized filtering.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,32 @@
-import PropTypes from 'prop-types';
-import { FormGroup, FormControlLabel, Checkbox } from "@mui/material";
+import PropTypes from "prop-types";
+import {
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  IconButton,
+  Stack,
+  Container,
+} from "@mui/material";
 import ExcelInput from "./ExcelInput";
+import SettingsIcon from "@mui/icons-material/Settings";
 import CompareButton from "./CompareButton";
 import { useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 export default function Form({ setShowResults, setResultsData }) {
-  const [ISF_NAC, setISF_NAC] = useState(false);
-  const [ONECA, setONECA] = useState(false);
   const [excelText, setExcelText] = useState("");
+  const [grantNames, setGrantNames] = useLocalStorage();
+  const [selectedGrants, setSelectedGrants] = useState([]);
 
   const handleSubmit = (event) => {
+    console.log(selectedGrants);
     event.preventDefault();
-    const selectTables = [];
-    if (ISF_NAC) {
-      selectTables.push("ISF_NAC Both");
-    }
-    if (ONECA) {
-      selectTables.push("ONECA-C");
-    }
-
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(
         tabs[0].id,
         {
           action: "extractData",
-          tables: selectTables,
+          tables: selectedGrants,
           excelText: excelText,
         },
         (response) => {
@@ -35,29 +37,56 @@ export default function Form({ setShowResults, setResultsData }) {
     });
   };
 
+  const handleCheckboxChange = (grant) => {
+    setSelectedGrants((prevSelectedGrants) => {
+      if (prevSelectedGrants.includes(grant)) {
+        return prevSelectedGrants.filter(
+          (selectedGrant) => selectedGrant !== grant
+        );
+      } else {
+        return [...prevSelectedGrants, grant];
+      }
+    });
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <FormGroup>
-        <FormControlLabel
-          control={<Checkbox />}
-          label={"ISF_NAC Both"}
-          checked={ISF_NAC}
-          onClick={() => setISF_NAC(!ISF_NAC)}
-        />
-        <FormControlLabel
-          control={<Checkbox />}
-          label={"ONECA-C"}
-          checked={ONECA}
-          onClick={() => setONECA(!ONECA)}
-        />
-      </FormGroup>
-      <ExcelInput excelText={excelText} setExcelText={setExcelText} />
-      <CompareButton />
-    </form>
+    <Container maxWidth="sm" sx={{mt: 2}}>
+      {!grantNames ||
+        (grantNames.length === 0 && (
+          <Stack
+            direction="row"
+            alignItems="center"
+            textAlign="center"
+            spacing={0.5}
+            sx={{ justifyContent: "center", my: 4 }}
+          >
+            <p>Click </p>
+            <SettingsIcon style={{ color: "rgba(0, 0, 0, 0.54)" }} />
+            <p>to add a grant</p>
+          </Stack>
+        ))}
+      <form onSubmit={handleSubmit}>
+        <FormGroup>
+          {grantNames &&
+            grantNames.map((grant) => (
+              <FormControlLabel
+                control={<Checkbox />}
+                label={grant}
+                checked={selectedGrants.includes(grant)}
+                onClick={() => handleCheckboxChange(grant)}
+                key={grant}
+                sx={{height: "30px"}}
+              />
+            ))}
+        </FormGroup>
+        <ExcelInput excelText={excelText} setExcelText={setExcelText} />
+        <CompareButton color="warning" />
+      </form>
+    </Container>
   );
 }
 
 Form.propTypes = {
   setShowResults: PropTypes.func,
   setResultsData: PropTypes.func,
-}
+};
