8dc5a6d69def35f581e398db12b82e8542fd0b6e
aconstas <82304192+aconstas@users.noreply.github.com>
2024-05-04 21:03:20 -0700

    style: update width, horiz center

--- src/components/CompareButton.jsx
+++ src/components/CompareButton.jsx
@@ -1,8 +1,8 @@
 import { Button } from "@mui/material";
 
-export default function CompareButton() {
+export default function CompareButton({ color }) {
   return (
-    <Button variant="contained" type="submit" size="large" sx={{ mt: 6 }} color="warning">
+    <Button variant="contained" type="submit" size="large" sx={{ mt: 6, width: "50%", mx: "auto" }} color={color}>
       Compare
     </Button>
   );
